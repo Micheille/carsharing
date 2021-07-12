239cce10e17ffae6f7ec5372da4195d2cb3162c7
Alina Ger <a.gerash@bk.ru>
2021-07-13 01:00:57 +0400

    Remove unused store files

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,16 +1,11 @@
 import { combineReducers } from 'redux';
 
-//import { staticReducer } from './static/reducers';
-//import { dynamicReducer } from './dynamic/reducers';
 import { orderReducer } from './order/reducers';
-import { extrasReducer } from './extras/reducers';
 import { dataReducer } from './data/reducers';
 
 const rootReducer = combineReducers({
-	//order: staticReducer,
-	order: orderReducer,
-	data: dataReducer,
-	extras: extrasReducer,
+  order: orderReducer,
+  data: dataReducer,
 });
 
 export default rootReducer;
