a891d90386dc7ada305c9284485f18d36664ed49
Alina Ger <a.gerash@bk.ru>
2021-07-13 15:35:04 +0400

    Correct calculating price

--- src/pages/OrderPage/Overall/index.js
+++ src/pages/OrderPage/Overall/index.js
@@ -39,11 +39,13 @@ const calculatePrice = (
   }
   if (rate.rateTypeId.name.includes('Недельный')) {
     const weeks = differenceInWeeks(dateTo, dateFrom);
-    price = rate.price * weeks;
+    const daysLeft = differenceInDays(dateTo, dateFrom) % 7;
+    price = daysLeft ? rate.price * (weeks + 1) : rate.price * weeks;
   }
   if (rate.rateTypeId.name.includes('Суточный')) {
     const days = differenceInDays(dateTo, dateFrom);
-    price = rate.price * days;
+    const minutesLeft = differenceInMinutes(dateTo, dateFrom) % 1440;
+    price = minutesLeft ? rate.price * (days + 1) : rate.price * days;
   }
 
   if (isFullTank) price += 500;
