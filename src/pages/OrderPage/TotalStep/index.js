6b066040897c61a9665f5b972e15a6b008ae267b
Alina Ger <a.gerash@bk.ru>
2021-07-14 20:52:00 +0400

    Add car info to total step

--- src/pages/OrderPage/TotalStep/index.js
+++ src/pages/OrderPage/TotalStep/index.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+import { normalizeImageLink } from '../../../utils/normalizeImageLink';
+
 import './style.scss';
 
 export default function TotalStep(props) {
+  const { carData } = props;
+
   return (
     <div className='order-process-content__step total-step'>
-      <span>totals</span>
+      <div className='total-step__car'>
+        <div className='total-step__car-info'>
+          <div className='total-step__model'>{carData.name}</div>
+          <div className='total-step__number'>{carData.number}</div>
+          <div className='total-step__tank'>
+            <span className='total-step__field'>Топливо </span>
+            {carData.tank}
+          </div>
+          <div className='total-step__available'>
+            <span className='total-step__field'>Доступна с </span>
+            {}
+          </div>
+        </div>
+        <div className='total-step__car-image'>
+          <img src={`${normalizeImageLink(carData.thumbnail.path)}`} alt='' />
+        </div>
+      </div>
     </div>
   );
 }
